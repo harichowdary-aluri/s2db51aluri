34a1e6b2b87f199464cd85297b76c3d16749932c
harichowdary-aluri <aluriharikiran01@gmail.com>
2022-04-24 15:05:25 -0500

    updated

--- routes/mobile.js
+++ routes/mobile.js
@@ -1,6 +1,16 @@
 var express = require('express');
 const mobile_controller = require('../controllers/mobile');
 var router = express.Router();
+// A little function to check if we have an authorized user and continue on 
+//or
+// redirect to login. 
+const secured = (req, res, next) => {
+  if (req.user) {
+    return next();
+  }
+  req.session.returnTo = req.originalUrl;
+  res.redirect("/login");
+}
 
 /* GET home page. */
 /*
@@ -8,19 +18,19 @@ router.get('/', function(req, res, next) {
   res.render('mobiles', { title: 'Search Results' });
 });
 */
-/* GET detail costume page */ 
-router.get('/detail', mobile_controller.mobile_view_one_Page); 
+/* GET detail costume page */
+router.get('/detail', mobile_controller.mobile_view_one_Page);
 
-/* GET create costume page */ 
-router.get('/create', mobile_controller.mobile_create_Page); 
+/* GET create costume page */
+router.get('/create', mobile_controller.mobile_create_Page);
 
 /* GET costumes */
-router.get('/', mobile_controller.mobile_view_all_Page );
+router.get('/', mobile_controller.mobile_view_all_Page);
 
-/* GET create update page */ 
-router.get('/update', mobile_controller.mobile_update_Page); 
+/* GET create update page */
+router.get('/update', secured, mobile_controller.mobile_update_Page);
 
-/* GET delete costume page */ 
-router.get('/delete', mobile_controller.mobile_delete_Page); 
+/* GET delete costume page */
+router.get('/delete', mobile_controller.mobile_delete_Page);
 
 module.exports = router;
